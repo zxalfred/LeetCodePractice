870a67868d13a67639f1d89e52603397bfb0470b
zxalfred <zxsprout@foxmail.com>
2020-03-25 11:16:17 +0800

    滑动窗口最大值

--- 239.滑动窗口最大值.js
+++ 239.滑动窗口最大值.js
@@ -1,3 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=javascript
+ *
+ * [239] 滑动窗口最大值
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
 var maxSlidingWindow = function(nums, k) {
   const window = []
   const result = []
@@ -19,5 +31,5 @@ var maxSlidingWindow = function(nums, k) {
   
   return result
 }
+// @lc code=end
 
-maxSlidingWindow([1,3,1,2,0,5], 3)
