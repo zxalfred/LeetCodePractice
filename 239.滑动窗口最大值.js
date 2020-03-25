ce4e734c70beef1cbc9c3e57ac43a14376afb970
zxalfred <zxsprout@foxmail.com>
2020-03-25 14:13:49 +0800

    优化滑动窗口最大值

--- 239.滑动窗口最大值.js
+++ 239.滑动窗口最大值.js
@@ -16,12 +16,9 @@ var maxSlidingWindow = function(nums, k) {
   const l = nums.length
   
   for (let i = 0; i < l; i++) {
-    const winL = window.length
     if (window[0] + k <= i) window.shift()
-    for (let j = 0; j < winL; j++) {
-      if (nums[window[window.length - 1]] < nums[i]) {
-        window.pop()
-      }
+    while (nums[window[window.length - 1]] < nums[i]) {
+      window.pop()
     }
     window.push(i)
     if (i >= k - 1) {
--- test.js
+++ test.js
@@ -4,19 +4,16 @@ var maxSlidingWindow = function(nums, k) {
   const l = nums.length
   
   for (let i = 0; i < l; i++) {
-    const winL = window.length
     if (window[0] + k <= i) window.shift()
-    for (let j = 0; j < winL; j++) {
-      if (nums[window[window.length - 1]] < nums[i]) {
-        window.pop()
-      }
+    while (nums[window[window.length - 1]] < nums[i]) {
+      window.pop()
     }
     window.push(i)
     if (i >= k - 1) {
       result.push(nums[window[0]])
     }
   }
-  
+
   return result
 }
 
