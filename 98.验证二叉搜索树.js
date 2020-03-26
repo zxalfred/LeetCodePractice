0c20594817baa46adc560edee67c7643521321e6
zxalfred <zxsprout@foxmail.com>
2020-03-26 19:23:03 +0800

    使用中序遍历验证二叉搜索树

--- 98.验证二叉搜索树.js
+++ 98.验证二叉搜索树.js
@@ -16,22 +16,25 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+
 var isValidBST = function(root) {
-  const validate = (node, lower, upper) => {
-    if (!node) return true
+  let pre = -Infinity
+  
+  const inorder = (node) => {
+    if (node === null) return true
 
-    const val = node.val
-    
-    if (val >= upper) return false
-    if (val <= lower) return false
-
-    if (!validate(node.left, lower, val)) return false
-    if (!validate(node.right, val, upper)) return false
+    if (!inorder(node.left)) return false
+    if (node.val <= pre) {
+      return false
+    } else {
+      pre = node.val
+    }
+    if (!inorder(node.right)) return false
 
     return true
   }
-
-  return validate(root)
+  
+  return inorder(root)
 };
 // @lc code=end
 
