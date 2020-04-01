8a6d5f642ebedf3d77327b03bc9d6b684cc033a0
zxalfred <zxsprout@foxmail.com>
2020-04-01 19:43:03 +0800

    数独

---
+++ 36.有效的数独.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=36 lang=javascript
+ *
+ * [36] 有效的数独
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+
+};
+// @lc code=end
+
