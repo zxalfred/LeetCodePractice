4a67bcd66b368b114ae964132c6302db4126e44d
zxalfred <zxsprout@foxmail.com>
2020-04-01 23:22:29 +0800

    数独

--- 37.解数独.js
+++ 37.解数独.js
@@ -13,7 +13,7 @@ var isValid = function(board, row, col, c) {
   for (let i = 0; i < 9; i++) {
     if (board[i][col] !== '.' && board[i][col] === c) return false
     if (board[row][i] !== '.' && board[row][i] === c) return false
-    if (board[3 * ~~(row / 3) + i / 3][3 * ~~(col / 3) + i % 3] !== '.' && board[3 * ~~(row / 3) + i / 3][3 * ~~(col / 3) + i % 3] === c) return false
+    if (board[3 * ~~(row / 3) + ~~(i / 3)][3 * ~~(col / 3) + ~~(i % 3)] !== '.' && board[3 * ~~(row / 3) + ~~(i / 3)][3 * ~~(col / 3) + ~~(i % 3)] === c) return false
   }
   return true
 }
@@ -21,24 +21,25 @@ var isValid = function(board, row, col, c) {
 var solve = function(board) {
   const l = board.length
   for (let i = 0; i < l; i++) {
-    for (let j = 0; j < l; j++) {
+    for (let j = 0; j < board[0].length; j++) {
       if (board[i][j] === '.') {
         for (let val = 1; val <= 9; val++) {
-          if (isValid(board, i, j, c)) {
-            board[i][j] = c
+          if (isValid(board, i, j, String(val))) {
+            board[i][j] = String(val)
             if (solve(board)) {
               return true
             } else {
               board[i][j] = '.'
             }
           }
-          return false
         }
+        return false
       }
-      return true
     }
   }
+  return true
 }
+
 
 var solveSudoku = function(board) {
   if (board === null || board.length === 0) return
--- test.js
+++ test.js
@@ -1,32 +1,35 @@
-var merge = function(left, right) {
-  const result = []
-  let i = 0
-  let j = 0
+var isValid = function(board, row, col, c) {
+  for (let i = 0; i < 9; i++) {
+    if (board[i][col] !== '.' && board[i][col] === c) return false
+    if (board[row][i] !== '.' && board[row][i] === c) return false
+    if (board[3 * ~~(row / 3) + ~~(i / 3)][3 * ~~(col / 3) + ~~(i % 3)] !== '.' && board[3 * ~~(row / 3) + ~~(i / 3)][3 * ~~(col / 3) + ~~(i % 3)] === c) return false
+  }
+  return true
+}
 
-  while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      result.push(left[i])
-      i++
-    } else {
-      result.push(right[j])
-      j++
+var solve = function(board) {
+  const l = board.length
+  for (let i = 0; i < l; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] === '.') {
+        for (let val = 1; val <= 9; val++) {
+          if (isValid(board, i, j, String(val))) {
+            board[i][j] = String(val)
+            if (solve(board)) {
+              return true
+            } else {
+              board[i][j] = '.'
+            }
+          }
+          return false
+        }
+      }
+      return true
     }
   }
-
-  if (i === left.length) {
-    return result.concat(right.slice(j))
-  } else {
-    return result.concat(left.slice(i))
-  }
 }
 
-var mergeSort = function(nums) {
-  if (nums.length < 2) return nums
-  const middle = nums.length >> 1
-  const left = nums.slice(0, middle)
-  const right = nums.slice(middle)
+const a = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 
-  return merge(mergeSort(left), mergeSort(right))
-}
-
-console.log(mergeSort([4,234,1,2,9,10,3]))
+solve(a)
+console.log(a)
