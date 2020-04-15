47bafffe6a5832a0b146af365f04cddfbbd15013
zxalfred <zxsprout@foxmail.com>
2020-04-15 19:36:57 +0800

    水壶问题

---
+++ 365.水壶问题.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=365 lang=javascript
+ *
+ * [365] 水壶问题
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {boolean}
+ */
+var gcd = (a, b) => {
+  if (b === 0) {
+    return a
+  }
+  return gcd(b, a % b)
+}
+
+var canMeasureWater = function(x, y, z) {
+  if (x + y < z) return false
+  if (x === 0 || y === 0) {
+    return z === 0 || x + y === z
+  }
+  return z % gcd(x, y) === 0
+};
+// @lc code=end
+
