f278b3a166ecafbe1324f0e862dba1cef0aa01a9
zxalfred <zxsprout@foxmail.com>
2020-04-15 09:41:16 +0800

    LRU缓存机制

--- test.js
+++ test.js
@@ -1,66 +1,97 @@
-var addTwoNumbers = function(l1, l2) {
-  const stack1 = []
-  const stack2 = []
-  let resultStack = []
-  let current = null
-  let head = null
-  while (l1) {
-    stack1.push(l1.val)
-    l1 = l1.next
+// 链表节点
+class Node{
+  constructor(key,val){
+      this.key = key;
+      this.val = val;
   }
-  while (l2) {
-    stack2.push(l2.val)
-    l2 = l2.next
+}
+// 双链表
+class DoubleList{
+  // 初始化头、尾节点、链表最大容量
+  constructor(){
+      this.head = new Node(0,0);
+      this.tail = new Node(0,0);
+      this.size = 0;
+      this.head.next = this.tail;
+      this.tail.prev = this.head;
   }
-  
-  let flag = 0
-
-  while (stack1.length || stack2.length || flag) {
-    const item1 = stack1.pop() || 0
-    const item2 = stack2.pop() || 0
-    const sum = item1 + item2 + flag
-    flag = ~~(sum / 10)
-    const val = sum % 10
-    resultStack.push(val)
+  // 在链表头部添加节点
+  addFirst(node){
+      node.next = this.head.next;
+      node.prev = this.head;
+      this.head.next.prev = node;
+      this.head.next = node;
+      this.size++;
   }
-
-  while (resultStack.length) {
-    const val = resultStack.pop()
-    if (head) {
-      head = new ListNode(val)
-      current = head
-    } else {
-      current.next = new ListNode(val)
-      current = current.next
-    }
+  // 删除链表中存在的node节点
+  remove(node){
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+      this.size--;
   }
-
-  return head
+  // 删除链表中最后一个节点，并返回该节点
+  removeLast(){
+      // 链表为空
+      if(this.tail.prev == this.head){
+          return null;
+      }
+      let last = this.tail.prev;
+      this.remove(last);
+      return last;
+  }
+}
+/**
+* @param {number} capacity
+*/
+var LRUCache = function(capacity) {
+  this.cap = capacity;
+  this.map = new Map();
+  this.cache = new DoubleList();
 };
 
-const l1 = {
-  val: 7,
-  next: {
-    val: 2,
-    next: {
-      val: 4,
-      next: {
-        val: 3,
-        next: null
+/** 
+* @param {number} key
+* @return {number}
+*/
+LRUCache.prototype.get = function(key) {
+  let map = this.map;
+  if(!map.has(key)){
+      return -1;
+  }
+  let val = map.get(key).val;
+  // 最近访问数据置前
+  this.put(key,val);
+  return val;
+};
+
+/** 
+* @param {number} key 
+* @param {number} value
+* @return {void}
+*/
+LRUCache.prototype.put = function(key, value) {
+  let cache = this.cache;
+  let map = this.map;
+  let node = new Node(key,value);
+  if(map.has(key)){
+      // 删除旧的节点，新的插到头部
+      cache.remove(map.get(key));
+  }else{
+      if(this.cap == cache.size){
+          // 删除最后一个
+          let last = cache.removeLast();
+          map.delete(last.key);
       }
-    }
   }
-}
+  // 新增头部
+  cache.addFirst(node);
+  // 更新 map 映射
+  map.set(key,node);
+};
 
-const l2 = {
-  val: 5,
-  next: {
-    val: 6,
-    next: {
-      val: 4,
-      next: null
-    }
-  }
-}
-
-addTwoNumbers(l1, l2)
+/** 
+* Your LRUCache object will be instantiated and called as such:
+* var obj = new LRUCache(capacity)
+* var param_1 = obj.get(key)
+* obj.put(key,value)
+*/
