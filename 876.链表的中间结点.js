18962a50bf31307cc1e690b1a327fc2a4fc07ff5
zxalfred <zxsprout@foxmail.com>
2020-04-17 17:19:37 +0800

    链表中间节点

---
+++ 876.链表的中间结点.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=javascript
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let slow = head
+  let fast = head
+
+  while(fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
+};
+// @lc code=end
+
