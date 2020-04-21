2452b93ac3d7c1d42048f66fc571802a76db1a0b
zxalfred <zxsprout@foxmail.com>
2020-04-21 23:24:18 +0800

    将数组分成相等三部分

--- test.js
+++ test.js
@@ -1,19 +1,29 @@
-var numberOfSubarrays = function(nums, k) {
-  const l = nums.length
-  let result = 0
-  const odd = []
-  let cur = 0
-  for (let i = 0; i < l; i++) {
-    if (nums[i] & 1) odd[++cur] = i
+var canThreePartsEqualSum = function(A) {
+  const target = A.reduce((a, b) => a + b) / 3
+  if (target % 1) return false
+  const l = A.length
+  let i = 0
+  let sum = 0
+  while (i < l) {
+    sum += A[i]
+    if (sum === target) {
+      break
+    }
+    i++
   }
-  odd[0] = -1
-  odd[++cur] = l
-
-  for (let i = 1; i + k <= cur; ++i) {
-    result += (odd[i] - odd[i - 1]) * (odd[i + k] - odd[i + k - 1])
+  sum = 0
+  if (i >= l) return false
+  i++
+  while (i < l) {
+    sum += A[i]
+    if (sum === target) {
+      break
+    }
+    i++
   }
+  if (i >= l - 1) return false
 
-  return result
+  return true
 };
 
-console.log(numberOfSubarrays([0,0,1,0,1,0,0,0], 2))
+console.log(canThreePartsEqualSum([1,-1,1,-1]))
