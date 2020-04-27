99ec0e2fc999df9fe595ca31ad1d9736ef8c9599
zxalfred <zxsprout@foxmail.com>
2020-04-27 09:27:34 +0800

    和为s的连续正数序列

---
+++ 面试题57 - II. 和为s的连续正数序列.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} target
+ * @return {number[][]}
+ */
+var findContinuousSequence = function(target) {
+  let i = 1
+  let j = 2
+  const result = []
+  while (i < j) {
+    const sum = (i + j) * (j - i + 1) / 2
+    if (sum === target) {
+      const item = []
+      for (let a = i; a <= j; a++) {
+        item.push(a)
+      }
+      result.push(item)
+      i++
+      j++
+    } else if (sum < target) {
+      j++
+    } else {
+      i++
+    }
+  }
+
+  return result
+};
+
+console.log(findContinuousSequence(15))
