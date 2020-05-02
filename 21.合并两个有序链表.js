358cccc7c6e1b063e8b2477b519b1c729fc4ff26
zxalfred <zxsprout@foxmail.com>
2020-05-02 13:44:44 +0800

    优化合并两个有序链表

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -20,21 +20,13 @@
 var mergeTwoLists = function(l1, l2) {
   if (!l1 || !l2) return l1 || l2
 
-  let res
-  let cur = {}
-  while (l1 || l2) {
-    if (!l2 || (l1 && l1.val <= l2.val)) {
-      cur.next = l1
-      l1 = l1.next
-    } else {
-      cur.next = l2
-      l2 = l2.next
-    }
-    if (!res) res = cur.next
-    cur = cur.next
+  if (l1.val <= l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
   }
-
-  return res
 };
 // @lc code=end
 
