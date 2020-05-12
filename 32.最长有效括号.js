9cd4aea6b95ef4ef513f9b6c05e7984d02a545eb
zxalfred <zxsprout@foxmail.com>
2020-05-12 11:37:14 +0800

    最长有效括号

---
+++ 32.最长有效括号.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=javascript
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+  let max = 0
+  const dp = new Array(s.length)
+
+  for(let i = 1; i < s.length; i++) {
+    if (s[i] === ')') {
+      if (s[i - 1] === '(') {
+        dp[i] = (dp[i - 2] || 0) + 2
+      } else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] === '(') {
+        dp[i] = dp[i - 1] + (dp[i - dp[i - 1] - 2] || 0) + 2
+      }
+      max = Math.max(max, dp[i] || 0)
+    }
+  }
+  return max
+};
+// @lc code=end
+
