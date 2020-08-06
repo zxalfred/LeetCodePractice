13397fac29838e692028804388e05603867dd2fb
zxalfred <zxsprout@foxmail.com>
2020-08-06 15:13:13 +0800

    优化不同路径

--- 63.不同路径-ii.js
+++ 63.不同路径-ii.js
@@ -9,40 +9,21 @@
  * @param {number[][]} obstacleGrid
  * @return {number}
  */
-const uniquePathsWithObstacles = function(obstacleGrid) {
-  const row = obstacleGrid.length
-  const col = obstacleGrid[0] ? obstacleGrid[0].length : 0
-
-  // 特殊情况判断
-  if (obstacleGrid[0][0] === 1) return 0
-  if (obstacleGrid[row - 1][col - 1] === 1) return 0
-
-  // 初始化 dp 数组
-  const dp = []
-  for (let i = 0; i < row; i++) {
-    dp[i] = new Array(col).fill(0)
-  }
-  dp[0][0] = 1
-  for (let j = 1; j < col; j++) {
-    if (obstacleGrid[0][j] !== 1) {
-      dp[0][j] = dp[0][j - 1]
-    }
-  }
-  for (let i = 1; i < row; i++) {
-    if (obstacleGrid[i][0] !== 1) {
-      dp[i][0] = dp[i - 1][0]
-    }
-  }
-
-  for (let i = 1; i < row; i++) {
-    for (let j = 1; j < col; j++) {
-      if (obstacleGrid[i][j] !== 1) {
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+const uniquePathsWithObstacles = function (obstacleGrid) {
+  var n = obstacleGrid.length;
+  var m = obstacleGrid[0].length;
+  var result = Array(m).fill(0);
+  result[0] = 1;
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < m; j++) {
+      if (obstacleGrid[i][j] == 1) {
+        result[j] = 0;
+      } else if (j > 0) {
+        result[j] += result[j - 1];
       }
     }
   }
-
-  return dp[row - 1][col - 1]
+  return result[m - 1];
 };
 // @lc code=end
 
