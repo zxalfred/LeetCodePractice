0c1f2bad2fa4bb9a6fcb0db76d32a3a75012cefb
zxalfred <zxsprout@foxmail.com>
2020-11-05 15:05:35 +0800

    单词接龙

--- 127.单词接龙.js
+++ 127.单词接龙.js
@@ -1,3 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=127 lang=javascript
+ *
+ * [127] 单词接龙
+ */
+
+// @lc code=start
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
 var ladderLength = function(beginWord, endWord, wordList) {
   if (!endWord || !wordList.includes(endWord)) return 0
 
@@ -34,5 +47,5 @@ var ladderLength = function(beginWord, endWord, wordList) {
   }
   return 0
 }
+// @lc code=end
 
-console.log(ladderLength('qa', 'sq', ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"]))
