700f1eb9080798a968b146cd851a23462895d526
zxalfred <zxsprout@foxmail.com>
2020-11-05 09:44:35 +0800

    插入区间

--- 57.插入区间.js
+++ 57.插入区间.js
@@ -14,41 +14,38 @@ var insert = function(intervals, newInterval) {
   if (!intervals.length) return [newInterval]
   const result = []
   let i
+  let settled = false
 
   for (i = 0; i < intervals.length; i++) {
     const now = intervals[i]
     if (newInterval[0] < now[0]) {
       if (newInterval[1] < now[0]) {
-        result.push(newInterval, now)
-        break
+        if (settled) {
+          result.push(now)
+        } else {
+          result.push(newInterval, now)
+          settled = true
+        }
       } else if (newInterval[1] <= now[1]) {
         result.push([newInterval[0], now[1]])
-        break
+        settled = true
       }
     } else if (newInterval[0] <= now[1]) {
       newInterval[0] = now[0]
       if (newInterval[1] <= now[1]) {
         result.push(now)
-        break
-      } else if (i === intervals.length - 1) {
-        result.push(newInterval)
-        return result
+        settled = true
       }
     } else {
       result.push(now)
-      if (i === intervals.length - 1) {
-        result.push(newInterval)
-      }
     }
   }
 
-  for (i++; i < intervals.length; i++) {
-    result.push(intervals[i])
+  if (!settled) {
+    result.push(newInterval)
   }
 
   return result
 };
-
-
 // @lc code=end
 
