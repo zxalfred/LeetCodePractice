7ba5ac2f399f3d83f405d0afe7c4af73fc587f6f
zxalfred <zxsprout@foxmail.com>
2020-11-06 09:40:07 +0800

    根据二进制1数量排序

--- test.js
+++ test.js
@@ -1,40 +1,17 @@
-var ladderLength = function(beginWord, endWord, wordList) {
-  if (!endWord || !wordList.includes(endWord)) return 0
-  const map = new Map()
-  const { length: len } = beginWord
-
-  for (let i = 0; i < wordList.length; i++) {
-    for (let j = 0; j < len; j++) {
-      const word = wordList[i]
-      const key = `${word.substring(0,j)}*${word.substring(j + 1, len)}`
-      !map.has(key) && map.set(key, [])
-      map.get(key).push(word)
-    }
+const calcBits = (num) => {
+  if (map.has(num)) {
+    return map.get(num)
   }
-
-  const queue = [[beginWord, 1]]
-  const memo = {beginWord: true}
-  
-  while (queue.length) {
-    const currNode = queue.shift()
-    const [currWord, currLevel] = currNode
-    for (let i = 0; i < len; i++) {
-      const key = `${currWord.substring(0,i)}*${currWord.substring(i + 1, len)}`
-      if (map.has(key)) {
-        const words = map.get(key)
-        for (let j = 0; j < words.length; j++) {
-          const word = words[j]
-          if (word === endWord) return currLevel + 1
-          if (!memo[word]) {
-            memo[word] = true
-            queue.push([word, currLevel + 1])
-          }
-        }
-      }
+  let count = 0
+  let i = num
+  while(i) {
+    if (i & 1 === 1) {
+      count++
     }
+    i = i >> 1
   }
-
-  return 0
+  map.set(num, count)
+  return count
 }
 
-console.log(ladderLength('hot', 'dog', ["hot","dog"]))
+calcBits(8)
