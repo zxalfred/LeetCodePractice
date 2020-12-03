a9ca4d5457828baf56d5b69a92e3c39e1c59eadb
zxalfred <zxsprout@foxmail.com>
2020-12-03 10:07:37 +0800

    计数质数

---
+++ 204.计数质数.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=204 lang=javascript
+ *
+ * [204] 计数质数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+  if (n <= 2) return 0
+  let count = 1
+  for (let i = 3; i < n; i++) {
+    const mid = Math.sqrt(i)
+    let isPrime = true
+    for (let j = 2; j <= mid; j++) {
+      if (i % j === 0) {
+        isPrime = false
+        break
+      }
+    }
+    if (isPrime) count++
+  }
+  
+  return count
+};
+// @lc code=end
+
