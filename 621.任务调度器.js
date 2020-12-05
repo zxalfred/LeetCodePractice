bfbc81247cf0afd5d55647ed7ce03c8c3c2c1834
zxalfred <zxsprout@foxmail.com>
2020-12-05 16:05:21 +0800

    621

---
+++ 621.任务调度器.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=621 lang=javascript
+ *
+ * [621] 任务调度器
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+  const map = new Map()
+  let max = 0
+  let maxCount = 0
+  for (const x of tasks) {
+    map.set(x, (map.get(x) || 0) + 1)
+  }
+
+  for (const [,val] of map) {
+    if (val > max) {
+      max = val
+      maxCount = 1
+    } else if (val === max) {
+      maxCount++
+    }
+  }
+
+  return Math.max((max - 1) * (n + 1) + maxCount, tasks.length)
+};
+// @lc code=end
+
