a06d7c2b9b19dbc553b0063ba0d41fed64f9f3f8
zxalfred <zxsprout@foxmail.com>
2020-12-05 13:59:26 +0800

    分割数组为连续子序列优化

--- 659.分割数组为连续子序列.js
+++ 659.分割数组为连续子序列.js
@@ -10,27 +10,34 @@
  * @return {boolean}
  */
 var isPossible = function(nums) {
-  const map = new Map()
-  if (!map.has(x)) {
-    map.set(x, new MinPriorityQueue())
+  const countMap = new Map()
+  const endMap = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    countMap.set(nums[i], (countMap.get(nums[i]) || 0) + 1)
   }
-  for (const x of nums) {
-    if (map.has(x - 1)) {
-      const prevLength = map.get(x - 1).dequeue()['priority']
-      if (map.get(x - 1).isEmpty()) {
-        map.delete(x - 1)
+
+  for (let i = 0; i < nums.length; i++) {
+    const item = nums[i]
+    if (countMap.get(item) > 0) {
+      if (endMap.get(item - 1)) {
+        endMap.set(item - 1, endMap.get(item - 1) - 1)
+        endMap.set(item, (endMap.get(item) || 0) + 1)
+        countMap.set(item, countMap.get(item) - 1)
+      } else if (countMap.get(item + 1) > 0 && countMap.get(item + 2) > 0) {
+        endMap.set(item + 2, (endMap.get(item + 2) || 0) + 1)
+        countMap.set(item, countMap.get(item) - 1)
+        countMap.set(item + 1, countMap.get(item + 1) - 1)
+        countMap.set(item + 2, countMap.get(item + 2) - 1)
+      } else {
+        return false
       }
-      map.get(x).enqueue(x, prevLength + 1)
-    } else {
-      map.get(x).enqueue(x, 1)
     }
   }
-  for (const [key, value] of map.entries()) {
-    if (value.front()['priority'] < 3) {
-      return false
-    }
-  }
+
   return true
 };
+
+console.log(isPossible([1,2,3,4,4,5]))
 // @lc code=end
 
