0fb5b59a5801d294078803af171de3c92ebc4497
zxalfred <zxsprout@foxmail.com>
2020-12-12 14:17:18 +0800

    update

---
+++ 71.简化路径.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=javascript
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+
+};
+// @lc code=end
+
