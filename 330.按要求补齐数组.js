b3003ec43e33a43ea471c10d4b3072399c6e7141
zxalfred <zxsprout@foxmail.com>
2020-12-29 17:52:30 +0800

    330

---
+++ 330.按要求补齐数组.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=330 lang=javascript
+ *
+ * [330] 按要求补齐数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+var minPatches = function(nums, n) {
+  let result = 0;
+  let x = 1;
+  const length = nums.length;
+  let index = 0;
+  while (x <= n) {
+      if (index < length && nums[index] <= x) {
+          x += nums[index];
+          index++;
+      } else {
+          x *= 2;
+          result++;
+      }
+  }
+  return result;
+};
+
+// @lc code=end
+
