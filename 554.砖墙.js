8ba869d00919fd45252990c1ce7ff0213ebf6328
zxalfred <zxsprout@foxmail.com>
2021-05-02 22:22:06 +0800

    554

--- 554.砖墙.js
+++ 554.砖墙.js
@@ -12,20 +12,15 @@
 var leastBricks = function(wall) {
   const l = wall.length
   const map = new Map()
-  let allSingle = true
   for (let i = 0; i < l; i++) {
     const item = wall[i]
-    let prev = item[0]
-    let count = map.get(prev)
-    map.set(prev, count ? count + 1 : 1)
-    if (item.length > 1) allSingle = false
-    for (let j = 1; j < item.length - 1; j++) {
+    let prev = 0
+    for (let j = 0; j < item.length - 1; j++) {
       prev = prev + item [j]
-      count = map.get(prev)
+      const count = map.get(prev)
       map.set(prev, count ? count + 1 : 1)
     }
   }
-  if (allSingle) return l
   const arr = [...map]
   let result = 0
   for (let i = 0; i < arr.length; i++) {
