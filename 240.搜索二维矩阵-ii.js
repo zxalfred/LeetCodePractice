782aa712ecde61ac598dec7b6ea51a2e8636f476
zxalfred <zxsprout@foxmail.com>
2021-05-03 11:26:02 +0800

    update

--- 240.搜索二维矩阵-ii.js
+++ 240.搜索二维矩阵-ii.js
@@ -13,42 +13,17 @@
 var searchMatrix = function(matrix, target) {
   let row = matrix.length
   let col = matrix[0] ? matrix[0].length : 0
-  let start = 0
-
-  while (start <= Math.min(row, col) - 1) {
-    let left = start
-    let right = row - 1
-    if (matrix[left][start] === target) return true
-    if (matrix[right][start] === target) return true
-    while (true) {
-      let mid = ~~((left + right) >> 1)
-      if (mid === left) break
-      const val = matrix[mid][start]
-      if (val === target) return true
-      if (target > val) {
-        left = mid
-      } else {
-        right = mid
-      }
+  
+  let i = row - 1
+  let j = 0
+  while (i < row && i >= 0 && j >= 0 && j < col) {
+    const val = matrix[i][j]
+    if (val === target) return true
+    if (val > target) {
+      i--
+    } else {
+      j++
     }
-    row = right + 1
-    left = start
-    right = col - 1
-    if (matrix[start][right] === target) return true
-    while (true) {
-      let mid = ~~((left + right) >> 1)
-      if (mid === left) break
-      const val = matrix[start][mid]
-      if (val === target) return true
-      if (target > val) {
-        left = mid
-      } else {
-        right = mid
-      }
-    }
-    col = right + 1
-    
-    start++
   }
 
   return false
