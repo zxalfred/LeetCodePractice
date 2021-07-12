35a2a5688575c0bbae85d13361a7c692c9b4369c
zxalfred <zxsprout@foxmail.com>
2021-07-12 20:53:29 +0800

    275

---
+++ 275.h-指数-ii.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=275 lang=javascript
+ *
+ * [275] H 指数 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+// 0 1 3 3 5 6   6
+// 1 2
+// 0 1
+var hIndex = function(citations) {
+  const l = citations.length
+  if (!l) return 0
+  let left = 0
+  let right = l - 1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
+    if (citations[mid] >= l - mid) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+
+  return l - left
+};
+
+// @lc code=end
+
