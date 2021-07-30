4ead228426357be9d6daf4d40245e9db43f0338d
zxalfred <zxsprout@foxmail.com>
2021-07-30 14:57:05 +0800

    1

---
+++ 1.两数之和.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const map = new Map()
+  for (const i in nums) {
+    const diff = target - nums[i]
+    if (map.has(diff)) {
+      return [map.get(diff), Number(i)]
+    }
+    map.set(nums[i], i)
+  }
+};
+// @lc code=end
+
