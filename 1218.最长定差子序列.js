2201fb4f4c8f30f62fa9734f0fb29013d7abb675
alfred.zhao <alfred.zhao@ximalaya.com>
2021-11-05 10:50:36 +0800

    1218

---
+++ 1218.最长定差子序列.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1218 lang=javascript
+ *
+ * [1218] 最长定差子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @param {number} difference
+ * @return {number}
+ */
+var longestSubsequence = function(arr, difference) {
+  const l = arr.length
+  let result = 1
+  const map = new Map([[arr[0], 1]])
+  for (let i = 1; i < l; i++) {
+    const prevMaxVal = map.get(arr[i] - difference)
+    const val = (prevMaxVal || 0) + 1
+    map.set(arr[i], val)
+    result = Math.max(result, val)
+  }
+  return result
+};
+
+// @lc code=end
+
