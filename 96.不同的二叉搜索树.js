e9ada0d4774e14f46435a44c2e8fa48de2ae0cd3
alfred.zhao <alfred.zhao@ximalaya.com>
2021-11-14 12:18:57 +0800

    96

---
+++ 96.不同的二叉搜索树.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=96 lang=javascript
+ *
+ * [96] 不同的二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  const dp = [0, 1, 2]
+
+  for (let i = 3; i <= n; i++) {
+    let result = 2 * dp[i - 1]
+    for (let j = 1; j <= i - 1; j++) {
+      const left = j - 1
+      const right = i - j
+      result += dp[left] * dp[right]
+    }
+    dp[i] = result
+  }
+
+  return dp[n]
+};
+// @lc code=end
+
