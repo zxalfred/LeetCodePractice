45c4c55b55b1a04730f9bc0e8921bd8cca071cff
alfred.zhao <alfred.zhao@ximalaya.com>
2021-11-14 12:07:52 +0800

    80

---
+++ 80.删除有序数组中的重复项-ii.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=80 lang=javascript
+ *
+ * [80] 删除有序数组中的重复项 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  const l = nums.length
+  if (l < 2) return l
+  let slow = 1
+  let fast = 1
+
+  while (fast < l) {
+    if (nums[fast] != nums[slow - 2]) {
+      nums[slow] = nums[fast]
+      slow++
+    }
+    fast++
+  }
+
+  return slow
+};
+// @lc code=end
+
