6ad661d43355a5774067604fb8f16e611c36e076
zxalfred <zxsprout@foxmail.com>
2021-12-04 23:23:48 +0800

    461

---
+++ src/461.汉明距离.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=461 lang=javascript
+ *
+ * [461] 汉明距离
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+  let i = x ^ y
+  let result = 0
+
+  while (i > 0) {
+    if ((i & 1) === 1) result++
+    i = i >> 1
+  }
+
+  return result
+};
+// @lc code=end
+
